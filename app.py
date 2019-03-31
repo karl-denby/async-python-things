74e1723e58e3fa5d8239f67a2843540479c1293b
karl-denby <karl.denby@gmail.com>
2019-03-31 22:49:08 +0100

    add websocket client

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from bocadillo import App
 
 app = App()
 
-@app.route("/")
-async def index(req, res):
-    res.text = "Hello, world!"
+@app.websocket_route("/conversation")
+async def converse(ws):
+    async for message in ws:
+        await ws.send(message)
 
 if __name__ == "__main__":
     app.run()
