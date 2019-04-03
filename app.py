6df97b83c386bc871317e10dd58e05c2fcadee54
karl-denby <karl.denby@gmail.com>
2019-04-03 20:39:39 +0100

    api endpoint

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ async def converse(ws, diego, save_client):
             response = diego.get_response(message)
             await ws.send(str(response))
 
+@app.route("/client-count")
+async def client_count(req, res, clients):
+    res.media = {"count": len(clients)}
+
 if __name__ == "__main__":
     app.run()
