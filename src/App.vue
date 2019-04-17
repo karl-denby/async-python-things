0cd41fc3a804d98b62b214658f954b6bbaf0bceb
Karl Denby <karl.denby@gmail.com>
2019-04-17 17:18:59 +0100

    Integrated sample UI

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Count msg="Number of clients is: "/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Count from './components/Count.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Count
   }
 }
 </script>
---
+++ src/components/Count.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="count">
+    <h1 v-on:click="fetchCount()">{{ msg }}{{ count }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Count',
+  data () {
+    return { 
+      count: null
+    }
+  },
+  mounted () {
+    this.fetchCount()
+  },
+  props: {
+    msg: String
+  },
+  methods: {
+    fetchCount() {
+      this.axios
+        .get('/client-count')
+        .then(response => this.count = response.data.count)
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
